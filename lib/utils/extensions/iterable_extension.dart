66050eb1f4f2e86008a2962750c6b89ed995c23b
boginni <brunnomarqueszr1@gmail.com>
2023-09-09 06:50:49 -0300

    feat: Implemented the following functions for IterableExtensions - nextInLine - nextInLineOrNull

--- lib/utils/extensions/iterable_extension.dart
+++ lib/utils/extensions/iterable_extension.dart
@@ -9,4 +9,28 @@ extension IterableExtensions<T> on Iterable<T> {
     }
     return null;
   }
+
+  T nextInLine(T element) {
+    var list = toList();
+    var currentIndex = list.indexOf(element);
+
+    if (currentIndex == -1) {
+      throw Exception();  // Element not found.
+    } else {
+      var nextIndex = (currentIndex + 1) % list.length;  // Use modulo for wrap-around.
+      return list[nextIndex];
+    }
+  }
+
+  T? nextInLineOrNull(T element) {
+    var list = toList();
+    var currentIndex = list.indexOf(element);
+
+    if (currentIndex == -1) {
+      return null;  // Element not found.
+    } else {
+      var nextIndex = (currentIndex + 1) % list.length;  // Use modulo for wrap-around.
+      return list[nextIndex];
+    }
+  }
 }
